16548e2d018a9201f3f4db1742ba39a2740ea055
Steven Seifried <gitlab@canox.net>
2020-03-12 22:50:32 +0100

    Change @ to [at]

--- systeminfos.sh
+++ systeminfos.sh
@@ -11,7 +11,7 @@ fi
 if [ -z $ticketnumber ]; then
     echo "Wie lautet Ihre Ticketnummer? Mit [ENTER] bestätigen / What is your ticket number?"
     echo "Bitte beachten Sie, dass wir ohne Ticketnummer, Ihr Anliegen nicht bearbeiten können. / We cannot proceed your inquire without ticket number!"
-    echo "Um eine Ticketnummer zu erhalten, schreiben Sie uns eine Mail an tux@tuxedocomputer.com mit Ihrem Anliegen. / To get an ticket number you can contact us by mail to tux@tuxedocomputers.com"
+    echo "Um eine Ticketnummer zu erhalten, schreiben Sie uns eine Mail an tux[at]tuxedocomputer.com mit Ihrem Anliegen. / To get an ticket number you can contact us by mail to tux[at]tuxedocomputers.com"
     echo "Sie müssen die Systeminfos nicht manuell an den Support senden, dies geschieht automatisch. / You do not have to send the system information to support manually, this happens automatically."
     echo " "
     while [ -z $ticketnumber ]; do
