b8d3154ae4bb3bc6e77fc0051325118abf3dfc75
Steven Seifried <gitlab@canox.net>
2022-07-27 13:02:36 +0200

    Moved Tomte configuration into the right section

--- systeminfos.sh
+++ systeminfos.sh
@@ -113,6 +113,21 @@ tuxedo-tomte list >> $infoFileName
 
 printf "\n\n\n" >> $infoFileName
 
+if [ -f /etc/tomte/AUTOMATIC ]; then
+    printf "Tomte wird in den vorgesehenen Standardeinstellungen verwendet\n" >> $infoFileName
+    printf "\n\n\n" >> $infoFileName
+elif [ -f /etc/tomte/DONT_CONFIGURE ]; then
+    printf "Tomte ist so konfiguriert dass nur die als "notwendig" (prerequisite) markierten Module konfiguriert werden\n" >> $infoFileName
+    printf "\n\n\n" >> $infoFileName
+elif [ -f /etc/tomte/UPDATES_ONLY ]; then
+    printf "Tomte ist so konfiguriert dass nur Aktualisierungen ueber Tomte verarbeitet werden\n" >> $infoFileName
+    printf "\n\n\n" >> $infoFileName
+else
+    printf "Tomte wird in den Standardeinstellungen verwendet\n" >> $infoFileName
+    printf "\n\n\n" >> $infoFileName
+fi
+
+
 printf "lsusb\n\n" >> $infoFileName
 lsusb >> $infoFileName
 
@@ -271,21 +286,6 @@ else
     printf "Maybe Tomte is not installed." >> $logFileName
     printf "\n\n\n" >> $logFileName
 
-fi
-
-
-if [ -f /etc/tomte/AUTOMATIC ]; then
-    printf "Tomte wird in den vorgesehenen Standardeinstellungen verwendet\n" >> $logFileName
-    printf "\n\n\n" >> $logFileName
-elif [ -f /etc/tomte/DONT_CONFIGURE ]; then
-    printf "Tomte ist so konfiguriert dass nur die als "notwendig" (prerequisite) markierten Module konfiguriert werden\n" >> $logFileName
-    printf "\n\n\n" >> $logFileName
-elif [ -f /etc/tomte/UPDATES_ONLY ]; then
-    printf "Tomte ist so konfiguriert dass nur Aktualisierungen ueber Tomte verarbeitet werden\n" >> $infoFileName
-    printf "\n\n\n" >> $logFileName
-else
-    printf "Tomte wird in den Standardeinstellungen verwendet\n" >> $logFileName
-    printf "\n\n\n" >> $logFileName
 fi
 
 printf "/var/log/syslog\n\n" >> $logFileName
