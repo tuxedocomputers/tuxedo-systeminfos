956d77587faace4913a881b8023aea276b278b57
Steven Seifried <gitlab@canox.net>
2022-07-27 12:28:27 +0200

    Fixed typo

--- systeminfos.sh
+++ systeminfos.sh
@@ -279,6 +279,9 @@ if [ -f /etc/tomte/UPDATES_ONLY ]; then
 elif [ -f /etc/tomte/DONT_CONFIGURE ]; then
     printf "Tomte ist so konfiguriert dass nur die als "notwendig" (prerequisite) markierten Module konfiguriert werden\n" >> $logFileName
     printf "\n\n\n" >> $logFileName
+elif [ -f /etc/tomte/AUTOMATIC ]; then
+    printf "Tomte wird in den vorgesehenen Standardeinstellungen verwendet\n" >> $logFileName
+    printf "\n\n\n" >> $logFileName
 else
     printf "Tomte wird in den Standardeinstellungen verwendet\n" >> $logFileName
     printf "\n\n\n" >> $logFileName
